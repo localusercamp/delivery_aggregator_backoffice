142e82c74f903162907d1e0af25d0682c914091a
localusercamp <45385480+localusercamp@users.noreply.github.com>
2021-03-08 14:07:54 +0500

    From define to getters

--- classes/User.js
+++ classes/User.js
@@ -7,35 +7,31 @@ export default class User extends Entity
   {
     super();
     Object.assign(this, user);
-
-    Object.defineProperty(this, 'isAdmin', {
-      get: () => this.role_id === Role.ADMIN,
-      ...User.prop_protected(),
-    });
-    Object.defineProperty(this, 'isModerator', {
-      get: () => this.role_id === Role.MODERATOR,
-      ...User.prop_protected(),
-    });
-    Object.defineProperty(this, 'isProvider', {
-      get: () => this.role_id === Role.PROVIDER,
-      ...User.prop_protected(),
-    });
-    Object.defineProperty(this, 'isClient', {
-      get: () => this.role_id === Role.CLIENT,
-      ...User.prop_protected(),
-    });
-
-    Object.defineProperty(this, 'startRoute', {
-      get: () => {
-        if (this.isProvider) {
-          if (this.company.is_approved) {
-            return '/dashboard';
-          }
-          return '/getting-started';
-        }
+  }
+  
+  get isAdmin() {
+    return this.role_id === Role.ADMIN;
+  }
+  
+  get isModerator() {
+    return this.role_id === Role.MODERATOR;
+  }
+  
+  get isProvider() {
+    return this.role_id === Role.PROVIDER;
+  }
+  
+  get isClient() {
+    return this.role_id === Role.CLIENT;
+  }
+  
+  get startRoute() {
+    if (this.isProvider) {
+      if (this.company.is_approved) {
         return '/dashboard';
-      },
-      ...User.prop_protected(),
-    });
+      }
+      return '/getting-started';
+    }
+    return '/dashboard';
   }
 }
